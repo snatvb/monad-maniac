68c03f6d5c4fa3b0b72cac96b6d99668ffe34270
Andrew Brigthon <snatvb@ya.ru>
2019-05-17 18:15:29 +0300

    Added few tests

---
+++ tests/maybe.test.ts
@@ -0,0 +1,68 @@
+import { Maybe } from '../src'
+
+const double = (x: number): number => x * 2
+const toNothing = (): undefined => undefined
+
+describe('Pure functions', () => {
+
+  describe('Maybe.of', () => {
+    it('with null', () => {
+      const nothing = Maybe.of(null)
+      expect(nothing.toString()).toBe('Nothing()')
+    })
+
+    it('with undefined', () => {
+      const nothing = Maybe.of(undefined)
+      expect(nothing.toString()).toBe('Nothing()')
+    })
+
+    it('with number', () => {
+      const just = Maybe.of(10)
+      expect(just.toString()).toBe('Just(10)')
+    })
+
+    it('with string', () => {
+      const just = Maybe.of('foo')
+      expect(just.toString()).toBe('Just(foo)')
+    })
+
+    it('with object', () => {
+      const just = Maybe.of({ foo: 'bar' })
+      expect(just.toString()).toBe('Just([object Object])')
+    })
+  })
+})
+
+describe('Just', () => {
+  it('map', () => {
+    const just = Maybe.of(5)
+    expect(just.toString()).toBe('Just(5)')
+    expect(just.map(double).toString()).toBe('Just(10)')
+    expect(just.map(double).map(double).toString()).toBe('Just(20)')
+    expect(just.map(double).map(double).map(toNothing).toString()).toBe('Nothing()')
+    expect(
+      just
+        .map(double)
+        .map(double)
+        .map(toNothing)
+        .map(double)
+        .toString()
+    ).toBe('Nothing()')
+  })
+
+  it('getOrElse', () => {
+    const just = Maybe.of(5)
+    expect(just.toString()).toBe('Just(5)')
+    expect(just.map(double).getOrElse('No value')).toBe(10)
+    expect(just.map(double).map(toNothing).getOrElse('No value')).toBe('No value')
+    expect(just.map(double).map(toNothing).getOrElse(null)).toBe(null)
+  })
+
+  // it('getOrElse', () => {
+  //   const just = Maybe.of(5)
+  //   expect(just.toString()).toBe('Just(5)')
+  //   expect(just.map(double).getOrElse('No value')).toBe(10)
+  //   expect(just.map(double).map(toNothing).getOrElse('No value')).toBe('No value')
+  //   expect(just.map(double).map(toNothing).getOrElse(null)).toBe(null)
+  // })
+})
