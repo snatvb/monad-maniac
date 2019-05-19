6e56bf83e251da0baa00e27c42c0c930ba0d4df4
Andrew Brigthon <snatvb@ya.ru>
2019-05-19 19:44:43 +0300

    Implemented filter, get, getOrElse

--- src/either.ts
+++ src/either.ts
@@ -9,7 +9,7 @@ export type CaseOf<L, R> = {
 export interface Shape<L, R> {
   map<U>(f: (value: R) => U): Shape<L, U>
   chain<U>(f: (value: R) => U): U | L
-  filter(f: (value: R) => boolean): Shape<L, R>
+  filter(predicate: (value: R) => boolean): Shape<L | R, R >
   getOrElse<U>(defaultValue: U): R | U
   get(): L | R
   toString(): string
@@ -44,14 +44,17 @@ export class Right<L ,R> implements Shape<L ,R> {
     return f(this.value)
   }
 
-  filter(f: (value: R) => boolean): Shape<L, R> {
-    throw new Error("Method not implemented.");
+  filter(predicate: (value: R) => boolean): Shape<L | R, R > {
+    if (predicate(this.value) === true) {
+      return this as Shape<L, R>
+    }
+    return new Left<R, R>(this.value)
   }
-  getOrElse<U>(defaultValue: U): R | U {
-    throw new Error("Method not implemented.");
+  getOrElse<U>(_defaultValue: U): R | U {
+    return this.value
   }
   get(): L | R {
-    throw new Error("Method not implemented.");
+    return this.value
   }
   toString(): string {
     throw new Error("Method not implemented.");
@@ -82,14 +85,14 @@ class Left<L, R> implements  Shape<L, R> {
     return this.value
   }
 
-  filter(f: (value: R) => boolean): Shape<L, R> {
-    throw new Error("Method not implemented.");
+  filter(_predicate: (value: R) => boolean): Shape<L | R, R > {
+    return this
   }
   getOrElse<U>(defaultValue: U): R | U {
-    throw new Error("Method not implemented.");
+    return defaultValue
   }
   get(): L | R {
-    throw new Error("Method not implemented.");
+    return this.value
   }
   toString(): string {
     throw new Error("Method not implemented.");
