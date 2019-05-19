3c8af49a374e7eae356a16cd310f53500345e2ae
Andrew Brigthon <snatvb@ya.ru>
2019-05-19 22:27:26 +0300

    Added map and rename Shape interface to Either
    Shape is not readable from type annotation
    Shape will type as alias for context Either.Shape

--- src/either.ts
+++ src/either.ts
@@ -1,4 +1,4 @@
-// import * as helpers from './helpers'
+import * as helpers from './helpers'
 // import { Nullable } from './types'
 
 export type CaseOf<L, R, U> = {
@@ -6,11 +6,14 @@ export type CaseOf<L, R, U> = {
   Left: (value: L) => U,
 }
 
-export interface Shape<L, R> {
-  map<U>(f: (value: R) => U): Shape<L, U>
+/** This is alias for normal display from context (`Either.Either` => `Either.Shape`) */
+export type Shape<L, R> = Either<L, R>
+
+export interface Either<L, R> {
+  map<U>(f: (value: R) => U): Either<L, U>
   orElse<U>(f: (value: L) => U): U | R
   chain<U>(f: (value: R) => U): U | L
-  filter(predicate: (value: R) => boolean): Shape<L | R, R >
+  filter(predicate: (value: R) => boolean): Either<L | R, R >
   getOrElse<U>(defaultValue: U): R | U
   get(): L | R
   toString(): string
@@ -19,25 +22,59 @@ export interface Shape<L, R> {
   caseOf<U>(matcher: CaseOf<L, R, U>): U
 }
 
-export function of<L, R>(value: R): Shape<L, R> {
+export function of<L, R>(value: R): Either<L, R> {
   return new Right(value)
 }
 
-export function fromNullable<L, R>(value: R | L): Shape<L, R> {
+export function fromNullable<L, R>(value: R | L): Either<L, R> {
   if (value === null || value === undefined) {
     return new Left<L, R>(value as L)
   }
   return new Right<L, R>(value as R)
 }
 
-export class Right<L ,R> implements Shape<L ,R> {
+/**
+ * Method like [`Either.map`](../interfaces/_either_.shape.html#map)
+ *
+ * ```ts
+ * import { Either } from 'monad-maniac'
+ *
+ * const name: string | void = Math.random() > 0.5 ? 'Jake' : undefined
+ * const result: Either.Shape<string, string> = typeof name === 'string'
+ *  ? new Either.Right(name)
+ * : new Either.Left('Server error')
+ * const greeting = Either.map((name) => `Welcome, ${name}!`, result)
+ *
+ * // ...
+ *
+ * if (greeting.isLeft()) {
+ *  console.error(greeting.get())
+ * } else {
+ *  showGreeting(greeting.get())
+ * }
+ *
+ * ```
+ * */
+export function map<L, R, U>(f: (value: R) => U, either: Either<L, R>): Either<L, U>
+/**
+ * Just curried `map`.
+ *
+ * _(a -> b) -> Either(a) -> Either(b)_
+ */
+export function map<L, R, U>(f: (value: R) => U): (either: Either<L, R>) => Either<L, U>
+export function map<L, R, U>(f: (value: R) => U, either?: Either<L, R>): Either<L, U> | ((either: Either<L, R>) => Either<L, U>) {
+  const op = (either: Either<L, R>) => either.map(f)
+  return helpers.curry1(op, either)
+}
+
+export class Right<L ,R> implements Either<L ,R> {
   private value: R
 
   constructor(value: R) {
     this.value = value
   }
 
-  map<U>(f: (value: R) => U): Shape<L, U> {
+  map<U>(f: (value: R) => U): Either<L, U> {
     return of(f(this.value))
   }
 
@@ -49,9 +86,9 @@ export class Right<L ,R> implements Shape<L ,R> {
     return f(this.value)
   }
 
-  filter(predicate: (value: R) => boolean): Shape<L | R, R > {
+  filter(predicate: (value: R) => boolean): Either<L | R, R > {
     if (predicate(this.value) === true) {
-      return this as Shape<L, R>
+      return this as Either<L, R>
     }
     return new Left<R, R>(this.value)
   }
@@ -75,14 +112,14 @@ export class Right<L ,R> implements Shape<L ,R> {
   }
 }
 
-class Left<L, R> implements  Shape<L, R> {
+export class Left<L, R> implements  Either<L, R> {
   private value: L
 
   constructor(value: L) {
     this.value = value
   }
 
-  map<U>(_f: (value: R) => U): Shape<L, U> {
+  map<U>(_f: (value: R) => U): Either<L, U> {
     return new Left<L, U>(this.value)
   }
 
@@ -94,7 +131,7 @@ class Left<L, R> implements  Shape<L, R> {
     return this.value
   }
 
-  filter(_predicate: (value: R) => boolean): Shape<L | R, R > {
+  filter(_predicate: (value: R) => boolean): Either<L | R, R > {
     return this
   }
   getOrElse<U>(defaultValue: U): R | U {
