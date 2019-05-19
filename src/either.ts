a762207bdb7a28349e91dbb87f7f4b193cca1754
Andrew Brigthon <snatvb@ya.ru>
2019-05-19 19:23:13 +0300

    Implemented map and chain

--- src/either.ts
+++ src/either.ts
@@ -1,4 +1,5 @@
-import { Nullable } from './types'
+// import * as helpers from './helpers'
+// import { Nullable } from './types'
 
 export type CaseOf<L, R> = {
   Right: (value: R) => L,
@@ -6,8 +7,8 @@ export type CaseOf<L, R> = {
 }
 
 export interface Shape<L, R> {
-  map<U>(f: (value: NonNullable<R>) => Nullable<U>): Shape<L, NonNullable<U>>
-  chain<U>(f: (value: R) => U): U | undefined
+  map<U>(f: (value: R) => U): Shape<L, U>
+  chain<U>(f: (value: R) => U): U | L
   filter(f: (value: R) => boolean): Shape<L, R>
   getOrElse<U>(defaultValue: U): R | U
   get(): L | R
@@ -15,4 +16,91 @@ export interface Shape<L, R> {
   isLeft(): boolean
   isRight(): boolean
   caseOf<U>(matcher: CaseOf<R, U>): U
+}
+
+export function of<L, R>(value: R): Shape<L, R> {
+  return new Right(value)
+}
+
+export function fromNullable<L, R>(value: R | L): Shape<L, R> {
+  if (value === null || value === undefined) {
+    return new Left<L, R>(value as L)
+  }
+  return new Right<L, R>(value as R)
+}
+
+export class Right<L ,R> implements Shape<L ,R> {
+  private value: R
+
+  constructor(value: R) {
+    this.value = value
+  }
+
+  map<U>(f: (value: R) => U): Shape<L, U> {
+    return of(f(this.value))
+  }
+
+  chain<U>(f: (value: R) => U): U | L {
+    return f(this.value)
+  }
+
+  filter(f: (value: R) => boolean): Shape<L, R> {
+    throw new Error("Method not implemented.");
+  }
+  getOrElse<U>(defaultValue: U): R | U {
+    throw new Error("Method not implemented.");
+  }
+  get(): L | R {
+    throw new Error("Method not implemented.");
+  }
+  toString(): string {
+    throw new Error("Method not implemented.");
+  }
+  isLeft(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  isRight(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  caseOf<U>(matcher: CaseOf<R, U>): U {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class Left<L, R> implements  Shape<L, R> {
+  private value: L
+
+  constructor(value: L) {
+    this.value = value
+  }
+
+  map<U>(_f: (value: R) => U): Shape<L, U> {
+    return new Left<L, U>(this.value)
+  }
+
+  chain<U>(_f: (value: R) => U): U | L {
+    return this.value
+  }
+
+  filter(f: (value: R) => boolean): Shape<L, R> {
+    throw new Error("Method not implemented.");
+  }
+  getOrElse<U>(defaultValue: U): R | U {
+    throw new Error("Method not implemented.");
+  }
+  get(): L | R {
+    throw new Error("Method not implemented.");
+  }
+  toString(): string {
+    throw new Error("Method not implemented.");
+  }
+  isLeft(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  isRight(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  caseOf<U>(matcher: CaseOf<R, U>): U {
+    throw new Error("Method not implemented.");
+  }
 }
