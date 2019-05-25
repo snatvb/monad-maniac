ee5b7c5a6bab6bf4e7b6b0fd4192df64d77f3c89
Andrew Brigthon <snatvb@ya.ru>
2019-05-25 13:03:01 +0300

    Added comment as docs for IO

--- src/io.ts
+++ src/io.ts
@@ -8,26 +8,41 @@ export class IO<T extends (...args: any[]) => any> implements Functor<T> {
     this.effect = effect
   }
 
+  /**
+   * This method running function from `IO` and result will be referred to
+   * `fn` function. Result from `fn` function will be wrapped up into `IO`.
+   */
   map<U>(fn: (value: ReturnType<T>) => U): IO<() => U> {
     return new IO(() => fn(this.effect()))
   }
 
+  /**
+   * Same like `map`, but...
+   * Result from `fn` function will **not** be wrapped up into `IO`.
+   */
   chain<U>(fn: (value: ReturnType<T>) => U): U {
     return fn(this.effect())
   }
 
+  /** To run function from `IO` */
   run() {
     return this.effect()
   }
 
 }
 
+/** This need just use with context `IO.Shape<T>` instead of `IO.IO<T>` */
 export interface Shape<T extends (...args: any[]) => any> extends IO<T> {}
 
+/**
+ * This function are getting some value and contains it
+ * in function where the function will returns the value (`() => value` )
+ * */
 export function of<T>(value: T) {
   return new IO(() => value)
 }
 
+/** Making `IO` monad from function */
 export function from(fn: (...args: any[]) => any) {
   return new IO(fn)
 }
